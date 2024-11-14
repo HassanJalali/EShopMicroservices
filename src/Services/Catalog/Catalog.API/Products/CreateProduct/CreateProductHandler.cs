0e8cf209cd2f7f568bf33e321f7e11cb10a904ab
jalali hassan <HassanJalali1996@outlook.com>
2024-11-14 20:00:20 +0330

    feat: implement CQRS using mediatr library for microservices

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CraeteProductCommand(string Name, List<string> Category, string Description,
+    string ImageFile, decimal Price) : ICommand<CraeteProductResult>;
+public record CraeteProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CraeteProductCommand, CraeteProductResult>
+{
+    public async Task<CraeteProductResult> Handle(CraeteProductCommand command, CancellationToken cancellationToken)
+    {
+        return new CraeteProductResult(Guid.NewGuid());
+    }
+}
+
